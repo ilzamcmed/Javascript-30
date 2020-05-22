c1378737b787d96a325cd5422330c6cc7167b7fb
Ilza <ilzammed2@gmail.com>
2020-05-21 23:02:17 -0300

    Desafio pronto

--- Javascript-Drum-Kit/js/script.js
+++ Javascript-Drum-Kit/js/script.js
@@ -1,6 +1,27 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
+
+    window.addEventListener('keydown', (e) => {
+
+        const audio = this.document.querySelector(`audio[data-key='${e.keyCode}']`)
+        this.console.log(audio)
+        audio.currentTime = 0;
+        audio.play()
+
+        const pressKey = this.document.querySelector(`.key[data-key='${e.keyCode}']`)
+        pressKey.classList.add('playing')
 
 
+        const removeEffect = (e) => {
+            if (e.propertyName !== 'transform') return;
+            console.log(e.propertyName)
+            pressKey.classList.remove('playing')
+        }
+
+        const allKeys = this.document.querySelectorAll('.key')
+        allKeys.forEach(key => key.addEventListener('transitionend', removeEffect));
+
+
+    })
 
 
 })
